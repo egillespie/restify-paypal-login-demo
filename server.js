18f271181fe503f6a786504f5d516ecb9213eb12
Erik Gillespie <erik.gillespie@gmail.com>
2016-05-11 11:26:52 -0400

    Added index.html

---
+++ server.js
@@ -0,0 +1,12 @@
+var restify = require('restify');
+
+var server = restify.createServer();
+
+server.get('/', restify.serveStatic({
+  directory: './static',
+  default: 'index.html'
+}));
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
