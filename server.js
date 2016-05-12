6fb1178b50baac167a9f07e8757b9a5a0f8c7618
Erik Gillespie <erik.gillespie@gmail.com>
2016-05-12 13:28:11 -0400

    Close login and log response on login

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var restify = require('restify');
 
 var server = restify.createServer();
 
-server.get('/', restify.serveStatic({
-  directory: './static',
-  default: 'index.html'
+server.use(restify.queryParser({ mapParams: false }));
+
+server.get('/auth/paypal/response', function(req, res, next) {
+  console.log(req.query);
+  res.json({ code: req.query.code });
+  return next();
+});
+
+// Try to serve a static HTML file if no other path has matched
+server.get(/\/?.*/, restify.serveStatic({
+  directory: __dirname,
+  match: /^.*index.html.*$/
 }));
 
 server.listen(8080, function() {
